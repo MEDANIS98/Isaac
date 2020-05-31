cee09cf992f50d7ed23943dd6a2b50e7e19e8516
CaddyDz <caddydz4@gmail.com>
2020-05-31 11:51:05 +0100

    Added 404 page
    Signed-off-by: CaddyDz <caddydz4@gmail.com>

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="card content-card">
+    <div class="card__content">
+        <div class="content-card__content">
+            <div class="row">
+                <div class="col-12 utility__text-center">
+                    <h1 class="text--primary-600 font-size-extra-large">404</h1>
+                    <h3>الصفحة التي طلبتها غير موجودة</h3>
+                    <br>
+                    <p>
+                        يمكنك استخدام حقل البحث في أعلى الموقع للبحث عن ما تريد، أو الانتقال
+                        <a href="/">للصفحة الرئيسية</a>.
+                    </p>
+                    <br>
+                    <br>
+                    <br>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@stop
