bd19b4aaba6bd62c4b8481f6717fcaec7234723d
Sadoudi Anis <sadoudimohamedanis@gmail.com>
2020-06-01 12:07:16 +0100

    Added interpretations route and view

---
+++ resources/views/interpretations.blade.php
@@ -0,0 +1,81 @@
+@extends('layouts.app')
+
+@section('title', __('Interpretation and reflection'))
+
+@section('content')
+<nav aria-label="breadcrumb" role="navigation">
+  <ol class="breadcrumb">
+    <li class="breadcrumb-item"><a href="/">الصفحة الرئيسية</a></li>
+    <li class="breadcrumb-item active" aria-current="page">التفسير والتدبر</li>
+  </ol>
+</nav>
+<div class="card content-card interpretations">
+  <div class="list-item-container">
+    <div class="row tafseer_category list-item">
+      <div class="col-12">
+        <div class="d-flex align-items-center utility__padding__left--2em">
+          <i
+            class="icon-category fa-lg utility__margin__left--10 utility__color--primary utility__margin__right--6"></i>
+          <a href="interpretations/category/366/%d9%85%d8%ac%d8%a7%d9%84%d8%b3-%d9%81%d9%8a-%d8%aa%d8%af%d8%a8%d8%b1-%d8%a7%d9%84%d9%82%d8%b1%d8%a7%d9%86-2.html"
+            class="utility__flex--1">
+            <p class="card__title card__title--no-border single-line">مجالس في تدبر القرآن</p>
+          </a>
+        </div>
+      </div>
+      <div class="col-12">
+        <div class="content-card__footer list-item__border-bottom">
+          <div class="row utility__no-padding no-gutters">
+            <div class="col-9">
+              <div class="utility__padding__top--14 row">
+                <div class="col">
+                  <span>
+                    عدد المواد (736)
+                  </span>
+                </div>
+                <div class="col">
+                  المشاهدات: 878032
+                </div>
+                <div class="col d-flex align-items-center">
+                </div>
+              </div>
+            </div>
+            <div class="col utility__text-left">
+              <div class="row no-gutters">
+                <div class="col utility__padding__top--14">
+                  <span class="favorites-count">3</span>
+                  <a href="#" data-favorite-id="19009" data-intro="إضافة إلى قائمة المفضلة" data-position="left"
+                    data-favorite-type="taxonomies" data-is-favorited="false">
+                    <i class="fa fa-heart-o utility__padding__right--8 hvr-pulse"></i>
+                  </a>
+                </div>
+                <div class="col-8 utility__padding__top--7">
+                  <div class="share-buttons">
+                    <a class="share-buttons__button share-buttons__button--facebook"
+                      href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fkhaledalsabt.com%2Finterpretations%2Fcategory%2F366%2F%25D9%2585%25D8%25AC%25D8%25A7%25D9%2584%25D8%25B3-%25D9%2581%25D9%258A-%25D8%25AA%25D8%25AF%25D8%25A8%25D8%25B1-%25D8%25A7%25D9%2584%25D9%2582%25D8%25B1%25D8%25A7%25D9%2586&amp;t=%D9%85%D8%AC%D8%A7%D9%84%D8%B3%20%D9%81%D9%8A%20%D8%AA%D8%AF%D8%A8%D8%B1%20%D8%A7%D9%84%D9%82%D8%B1%D8%A2%D9%86"
+                      target="_blank" title="شارك على فيسبوك"><i class="fa fa-facebook fa-fw"
+                        aria-hidden="true"></i><span class="sr-only">شارك على فيسبوك</span></a>
+                    <a class="share-buttons__button share-buttons__button--twitter"
+                      href="https://twitter.com/intent/tweet?source=https%3A%2F%2Fkhaledalsabt.com%2Finterpretations%2Fcategory%2F366%2F%25D9%2585%25D8%25AC%25D8%25A7%25D9%2584%25D8%25B3-%25D9%2581%25D9%258A-%25D8%25AA%25D8%25AF%25D8%25A8%25D8%25B1-%25D8%25A7%25D9%2584%25D9%2582%25D8%25B1%25D8%25A7%25D9%2586&amp;text=%D9%85%D8%AC%D8%A7%D9%84%D8%B3%20%D9%81%D9%8A%20%D8%AA%D8%AF%D8%A8%D8%B1%20%D8%A7%D9%84%D9%82%D8%B1%D8%A2%D9%86:%20https%3A%2F%2Fkhaledalsabt.com%2Finterpretations%2Fcategory%2F366%2F%25D9%2585%25D8%25AC%25D8%25A7%25D9%2584%25D8%25B3-%25D9%2581%25D9%258A-%25D8%25AA%25D8%25AF%25D8%25A8%25D8%25B1-%25D8%25A7%25D9%2584%25D9%2582%25D8%25B1%25D8%25A7%25D9%2586&amp;via=khaledalsabt"
+                      target="_blank" title="انشر تغريدة"><i class="fa fa-twitter fa-fw" aria-hidden="true"></i><span
+                        class="sr-only">انشر تغريدة</span></a>
+                    <a class="share-buttons__button share-buttons__button--google"
+                      href="https://plus.google.com/share?url=https%3A%2F%2Fkhaledalsabt.com%2Finterpretations%2Fcategory%2F366%2F%25D9%2585%25D8%25AC%25D8%25A7%25D9%2584%25D8%25B3-%25D9%2581%25D9%258A-%25D8%25AA%25D8%25AF%25D8%25A8%25D8%25B1-%25D8%25A7%25D9%2584%25D9%2582%25D8%25B1%25D8%25A7%25D9%2586"
+                      target="_blank" title="شارك على غوغل بلس"><i class="fa fa-google-plus fa-fw"
+                        aria-hidden="true"></i><span class="sr-only">شارك على غوغل بلس</span></a>
+                    <a class="share-buttons__button share-buttons__button--email"
+                      href="cdn-cgi/l/email-protection.html#99a6eaecfbf3fcfaeda4bcdda0bca1acbcdda1bcd8dabcdda1bcd8aebcdda0bca1adbcdda1bcdbaabcaba9bcdda0bca1a8bcdda0bca1d8bcaba9bcdda1bcd8d8bcdda1bcd8dfbcdda1bcd8a1bcdda1bcdba8bcaba9bcdda1bcd8aebcdda0bca1adbcdda0bca1abbcdda1bcdba8bcdda1bcd8abbcdda0bca1afbffbf6fde0a4daf1fcfaf2b9f6ecedb9edf1f0eab9edf6e9f0faa3bcaba9f1edede9eabcaad8bcabdfbcabdff2f1f8f5fcfdf8f5eaf8fbedb7faf6f4bcabdff0f7edfcebe9ebfcedf8edf0f6f7eabcabdffaf8edfcfef6ebe0bcabdfaaafafbcabdfbcabacdda0bcabaca1acbcabacdda1bcabacd8dabcabacdda1bcabacd8aebcabacdda0bcabaca1adbcabacdda1bcabacdbaab4bcabacdda0bcabaca1a8bcabacdda0bcabaca1d8b4bcabacdda1bcabacd8d8bcabacdda1bcabacd8dfbcabacdda1bcabacd8a1bcabacdda1bcabacdba8b4bcabacdda1bcabacd8aebcabacdda0bcabaca1adbcabacdda0bcabaca1abbcabacdda1bcabacdba8bcabacdda1bcabacd8aebcabacdda0bcabaca1af"
+                      target="_blank" title="أرسل بريدًا"><i class="fa fa-envelope fa-fw" aria-hidden="true"></i><span
+                        class="sr-only">أرسل بريدًا</span></a>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+<div class="utility__text-center">
+</div>
+@stop
