c3ac96b838592aa802856eaa087758bb1ccd4a08
Sadoudi Anis <sadoudimohamedanis@gmail.com>
2020-06-03 14:43:52 +0100

    explanation files

---
+++ resources/views/explanations/explanation.blade.php
@@ -0,0 +1,84 @@
+@extends('layouts.app')
+
+@section('title', __('Scientific annotations'))
+
+@section('content')
+
+<nav aria-label="breadcrumb" role="navigation">
+	<ol class="breadcrumb">
+		<li class="breadcrumb-item"><a href="https://khaledalsabt.com/">الصفحة الرئيسية</a></li>
+		<li class="breadcrumb-item"><a href="https://khaledalsabt.com/explanations">شروح الكتب</a></li>
+		<li class="breadcrumb-item"><a href="https://khaledalsabt.com/explanations/book/30/شرح-كتاب-رياض-الصالحين/index">شرح
+				كتاب رياض
+				الصالحين</a></li>
+		<li class="breadcrumb-item active" aria-current="page"> 13- باب في بيان كثرة طرق الخير</li>
+	</ol>
+</nav>
+<div class="hidden" id="history-add" data-type="post" data-id="139993"></div>
+<div class="hidden" id="stats-add-view" data-url="https://khaledalsabt.com/api/stats/view/139993/explainable_book">
+</div>
+<div class="card">
+	<div class="content-card book_explanation">
+		<div class="card__content__header card__content__header--flex">
+			<div class="card__content__header__title">
+				 13- باب في بيان كثرة طرق الخير
+			</div>
+			<a href="https://khaledalsabt.com/explanations/book/207/ 13--باب-في-بيان-كثرة-طرق-الخير/index"
+				class="btn btn--light">
+				فهارس الكتاب
+				<i class="fa fa-angle-double-left" style="font-size: 12px"></i>
+			</a>
+		</div>
+		<div class="row books_explanation list-item">
+			<div class="col-12">
+				<div class="d-flex align-items-center utility__padding__left--2em">
+					<i class="icon-sound fa-lg utility__margin__left--10 utility__color--primary utility__margin__right--6"></i>
+					<a href="https://khaledalsabt.com/explanations/1328/الحديث-على-ايات-الباب" class="utility__flex--1">
+						<p class="card__title card__title--no-border single-line">الحديث على آيات الباب</p>
+					</a>
+				</div>
+			</div>
+			<div class="col-12">
+				<div class="content-card__footer list-item__border-bottom">
+					<div class="row utility__no-padding no-gutters">
+						<div class="col-9">
+							<div class="utility__padding__top--14 row">
+								<div class="col">
+									المشاهدات: 2715
+								</div>
+								<div class="col d-flex align-items-center">
+									<a href="https://files.zadapps.info/khaledalsabt.com/migrated/02_drous/Riad_Assaleheen/013/Riad_Assaleheen_013_01.mp3"
+										class="content-card__meta--shrink-font utility__cursor--pointer stats-add-on-click stats-add-download hvr-pulse"
+										data-url="https://khaledalsabt.com/api/stats/download/136426/books_explanation" download>
+										<i class="fa fa-download utility__padding__left--8"></i>
+									</a>
+									مرات التحميل: 2038
+								</div>
+								<div class="col">
+									08 / شوّال / 1426
+								</div>
+							</div>
+						</div>
+						<div class="col utility__text-left">
+							<div class="row no-gutters">
+								<div class="col utility__padding__top--14">
+									<span class="favorites-count">0</span>
+									<a href="#" data-favorite-id="136426" data-intro="إضافة إلى قائمة المفضلة" data-position="left"
+										data-favorite-type="posts" data-is-favorited="false">
+										<i class="fa fa-heart-o utility__padding__right--8 hvr-pulse"></i>
+									</a>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		{{-- paginations --}}
+
+		<div class="col-lg-3 d-lg-block d-none">
+		</div>
+	</div>
+</div>
+@stop
