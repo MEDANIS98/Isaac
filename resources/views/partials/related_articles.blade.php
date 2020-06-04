21c78c434025e8f5c71a0039d2b1d1318ec59f62
Sadoudi Anis <sadoudimohamedanis@gmail.com>
2020-06-04 14:02:52 +0100

    related articles in lectures and explanations
    Signed-off-by: Sadoudi Anis <sadoudimohamedanis@gmail.com>

---
+++ resources/views/partials/related_articles.blade.php
@@ -0,0 +1,46 @@
+{{-- Related articles --}}
+
+<div class="heading">
+	<h2 class="heading__title">
+		مواد ذات صلة
+	</h2>
+</div>
+<div class="cards cards--space-between cards--small">
+	<div class="card card--cover">
+		<div class="cover" style="background-image: url('https://khaledalsabt.com/img/types/lecture.jpg')">
+			<span class="card__gradients"></span>
+		</div>
+		<div class="card__content interpretation">
+			<a
+				href="https://khaledalsabt.com/lectures/7/%D9%88%D9%82%D9%81%D8%A7%D8%AA-%D9%85%D8%B9-%D9%82%D9%88%D9%84%D9%87-%D8%AA%D8%B9%D8%A7%D9%84%D9%89-%D9%88%D9%84%D8%A7-%D8%AA%D9%82%D9%81-%D9%85%D8%A7-%D9%84%D9%8A%D8%B3-%D9%84%D9%83-%D8%A8%D9%87-%D8%B9%D9%84%D9%85">
+				<p class="card__title card__title--no-border card__title--font-sm">
+					وقفات مع قوله تعالى: (ولا تقف ما ليس لك به علم)
+				</p>
+			</a>
+		</div>
+	</div>
+	<div class="card card--cover">
+		<div class="cover" style="background-image: url('https://khaledalsabt.com/img/types/lecture.jpg')">
+			<span class="card__gradients"></span>
+		</div>
+		<div class="card__content interpretation">
+			<a href="https://khaledalsabt.com/lectures/3/%D8%A7%D9%84%D9%85%D8%A8%D8%A7%D9%84%D8%BA%D9%88%D9%86">
+				<p class="card__title card__title--no-border card__title--font-sm">
+					المبالغون
+				</p>
+			</a>
+		</div>
+	</div>
+	<div class="card card--cover">
+		<div class="cover" style="background-image: url('https://khaledalsabt.com/img/types/lecture.jpg')">
+			<span class="card__gradients"></span>
+		</div>
+		<div class="card__content interpretation">
+			<a href="https://khaledalsabt.com/lectures/5/%D8%A7%D8%AD%D9%83%D8%A7%D9%85-%D8%A7%D9%84%D8%AA%D8%B4%D8%A8%D9%87">
+				<p class="card__title card__title--no-border card__title--font-sm">
+					أحكام التشبه
+				</p>
+			</a>
+		</div>
+	</div>
+</div>
